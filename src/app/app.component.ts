f80e0a88f431e169dc085844e05d1c992731287b
solver-mathanraj <matheesh1214@gmail.com>
2025-04-06 09:42:53 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DisplayComponent } from './display/display.component';
 
 @Component({
   selector: 'app-root',
